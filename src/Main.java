db5bd31f7e76dde5f56d3fcf6b7846fd7f3c350b
Pilha-DS <jonathan.sanchescardoso@gmail.com>
2026-09-15 14:26:12 -0300

    primeira conexao

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
-import java.awt.*;
+import Network.ConnectHost;
+import Network.HostCreate;
 
-void main() {
-    View v = new View();
+void main() throws Exception {
+
+    HostCreate host = new HostCreate(
+            "1234",
+            "testename",
+            1,
+            5000
+    );
+
+    ConnectHost connection = new ConnectHost();
+
+    Thread hostThread = new Thread(() -> {
+        try {
+            host.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    });
+
+    hostThread.start();
+
+    while (!host.isReady()) {
+        Thread.sleep(10);
+    }
+
+    connection.connect(
+            "127.0.0.1",
+            5000,
+            "pilha"
+    );
+
+    System.out.println(
+            host.getRoom().getPlayers()
+    );
 }
